8aab98ede8785ee37461bc7b98f1994fcb617564
LawalGabriel <104921116+LawalGabriel@users.noreply.github.com>
2025-12-03 19:27:23 +0100

    Update to the employee dashboard tiles

--- src/webparts/goil/layout/Sidebar/SideBar.tsx
+++ src/webparts/goil/layout/Sidebar/SideBar.tsx
@@ -83,12 +83,12 @@ const SideBar: React.FC = () => {
           </NavLink>
         </div>
         {/* User side bar view */}
-        EmployeeDashboard
+        Employee Dashboard
         <div className={styles.navSection}>
-          <div className={styles.navSectionTitle}>User Dashboard</div>
-          <NavLink to="/EmployeeDashboard" className={getNavLinkClass}>
+          <div className={styles.navSectionTitle}>Employee Dashboard</div>
+          <NavLink to="/EmpDashboard" className={getNavLinkClass}>
             <Icon iconName="PlayerSettings"></Icon>
-            <span>User Dashboard</span>
+            <span>Employee Dashboard</span>
           </NavLink>
           <NavLink to="/department-management" className={getNavLinkClass}>
             <Icon iconName="EMI"></Icon>
